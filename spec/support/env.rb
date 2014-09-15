5e526cdb3d0700e4c4a32229363b62cab44737e1
Michael van Rooijen <michael@vanrooijen.io>
2014-09-15 11:28:56 +0200

    Removed DB_PATH.

--- spec/support/env.rb
+++ spec/support/env.rb
@@ -7,7 +7,6 @@ require "active_record"
 ROOT_PATH = File.expand_path("../../..", __FILE__)
 SPEC_PATH = File.join(ROOT_PATH, "spec")
 TMP_PATH = File.join(ROOT_PATH, "tmp")
-DB_PATH = File.join(TMP_PATH, "parelation.db")
 LOG_PATH = File.join(TMP_PATH, "parelation.log")
 
 FileUtils.mkdir_p(TMP_PATH)
