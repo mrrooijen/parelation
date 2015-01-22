3f3f3c197c1fdd5433eca9b3827c4de68b4c0abb
Michael van Rooijen <michael@vanrooijen.io>
2015-01-22 03:53:22 +0100

    Fixed indentation.

--- lib/parelation/criteria/where/directional_query_applier.rb
+++ lib/parelation/criteria/where/directional_query_applier.rb
@@ -1,53 +1,53 @@
-  class Parelation::Criteria::Where::DirectionalQueryApplier
+class Parelation::Criteria::Where::DirectionalQueryApplier
 
-    # @return [Hash] keyword to operator mappings
-    #
-    OPERATOR_MAPPING = {
-      "where_gt" => ">",
-      "where_gte" => ">=",
-      "where_lt" => "<",
-      "where_lte" => "<="
-    }
+  # @return [Hash] keyword to operator mappings
+  #
+  OPERATOR_MAPPING = {
+    "where_gt" => ">",
+    "where_gte" => ">=",
+    "where_lt" => "<",
+    "where_lte" => "<="
+  }
 
-    # @return [ActiveRecord::Relation]
-    #
-    attr_reader :chain
+  # @return [ActiveRecord::Relation]
+  #
+  attr_reader :chain
 
-    # @return [String]
-    #
-    attr_reader :operator
+  # @return [String]
+  #
+  attr_reader :operator
 
-    # @return [String]
-    #
-    attr_reader :field
+  # @return [String]
+  #
+  attr_reader :field
 
-    # @return [String]
-    #
-    attr_reader :value
+  # @return [String]
+  #
+  attr_reader :value
 
-    # @param chain [ActiveRecord::Relation] the chain to apply to
-    # @param operator [String] the named operator from the params
-    # @param field [String] the field to query on
-    # @param value [String] the value of the query
-    #
-    def initialize(chain, operator, field, value)
-      @chain = chain
-      @operator = operator
-      @field = field
-      @value = value
-    end
-
-    # @return [ActiveRecord::Relation] the chain with newly applied operations
-    #
-    def apply
-      chain.where(sql, field, value)
-    end
-
-    private
-
-    # @return [String] the base SQL template to build queries on-top of
-    #
-    def sql
-      %Q{"#{chain.arel_table.name}".? #{OPERATOR_MAPPING[operator]} ?}
-    end
+  # @param chain [ActiveRecord::Relation] the chain to apply to
+  # @param operator [String] the named operator from the params
+  # @param field [String] the field to query on
+  # @param value [String] the value of the query
+  #
+  def initialize(chain, operator, field, value)
+    @chain = chain
+    @operator = operator
+    @field = field
+    @value = value
   end
+
+  # @return [ActiveRecord::Relation] the chain with newly applied operations
+  #
+  def apply
+    chain.where(sql, field, value)
+  end
+
+  private
+
+  # @return [String] the base SQL template to build queries on-top of
+  #
+  def sql
+    %Q{"#{chain.arel_table.name}".? #{OPERATOR_MAPPING[operator]} ?}
+  end
+end
